3019c280913ae7ae4ae993f12be9b6c503b14e76
jktheking <jiten.kumar.jha@gmail.com>
2018-03-25 14:14:38 +0530

    initial commit

---
+++ src/main/java/edu/algods/AlgodsApplication.java
@@ -0,0 +1,12 @@
+package edu.algods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgodsApplication.class, args);
+	}
+}
